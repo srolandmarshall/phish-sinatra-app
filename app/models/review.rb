8fab712c4eb420e6c19ee0da74def52a28ac4915
Sam Marshall <samuel.r.marshall@gmail.com>
2017-11-25 21:20:58 -0500

    simplifying everything, I think

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,5 @@ class Review < ActiveRecord::Base
 
 belongs_to :show
 belongs_to :user
-has_many :show_reviews
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171126020503) do
+ActiveRecord::Schema.define(version: 20171126021842) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "text"
@@ -45,11 +45,6 @@ ActiveRecord::Schema.define(version: 20171126020503) do
     t.integer "year"
     t.integer "num_shows"
     t.string "link"
-  end
-
-  create_table "user_reviews", force: :cascade do |t|
-    t.integer "review_id"
-    t.integer "user_id"
   end
 
   create_table "user_shows", force: :cascade do |t|
