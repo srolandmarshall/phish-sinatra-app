7b05a77a75fad162669118f2c4331fa8e565e097
Sam Marshall <samuel.r.marshall@gmail.com>
2017-11-25 20:36:22 -0500

    moved show controls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,8 +24,6 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-
-
   #tours
 
   get '/tours' do
@@ -36,31 +34,6 @@ class ApplicationController < Sinatra::Base
     end
   end
 
-  #shows
-  get '/shows' do
-    erb :'shows/index'
-  end
 
-  get '/addshow' do
-    @years = []
-    Tour.all.each {|t| @years << t.year}
-    @years.uniq!
-    @user = current_user
-    if logged_in?
-      erb :'shows/add'
-    else
-      redirect '/login'
-    end
-  end
-
-  post '/addshow' do
-    @years = []
-    Tour.all.each {|t| @years << t.year}
-    @years.uniq!
-    @year = params[:year]
-    @shows = Show.where(year: params[:year])
-    @user = current_user
-    erb :'shows/add'
-  end
 
 end
---
+++ app/controllers/shows_controller.rb
@@ -0,0 +1,29 @@
+class ShowController < ApplicationController
+  #shows
+  get '/shows' do
+    erb :'shows/index'
+  end
+
+  get '/addshow' do
+    @years = []
+    Tour.all.each {|t| @years << t.year}
+    @years.uniq!
+    @user = current_user
+    if logged_in?
+      erb :'shows/add'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/addshow' do
+    @years = []
+    Tour.all.each {|t| @years << t.year}
+    @years.uniq!
+    @year = params[:year]
+    @shows = Show.where(year: params[:year])
+    @user = current_user
+    erb :'shows/add'
+  end
+
+end
